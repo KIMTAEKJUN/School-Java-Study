7bce30737740fe127e98eccd0adbabdba2aba160
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-03-31 14:04:03 +0900

    Initial commit

---
+++ Scanner/src/ScannerTest/ScannerEx.java
@@ -0,0 +1,14 @@
+package ScannerTest;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        int number = sc.nextInt();
+
+        System.out.println("이름은 " + name + "이고, 번호는 " + number + "번입니다.");
+    }
+}
