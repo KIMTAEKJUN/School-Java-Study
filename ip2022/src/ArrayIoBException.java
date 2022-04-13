07350781325b160bce2f673ddddb013632563942
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-04-13 14:01:48 +0900

    .

---
+++ ip2022/src/ArrayIoBException.java
@@ -0,0 +1,14 @@
+public class ArrayIoBException {
+    public static void main(String[] args) {
+        try {
+            String data1 = args[0];
+            String data2 = args[1];
+            System.out.println("args[0] : " + data1);
+            System.out.println("args[1] : " + data2);
+        } catch (Exception e) {
+            System.out.println("배열의 인덱스 값이 존재하지않습니다.");
+        } finally {
+            System.out.println("소스코드를 다시 한번 확인해주세요.");
+        }
+    }
+}
---
+++ ip2022/src/NumberFormatException.java
@@ -0,0 +1,18 @@
+public class NumberFormatException {
+    public static void main(String[] args) throws Exception {
+        String data1 = "100";
+        String data2 = "a100";
+
+        try {
+            int value1 = Integer.parseInt(data1);
+            int value2 = Integer.parseInt(data2);
+
+            int result = value1 + value2;
+            System.out.println(data1 + " + " + data2 + " = " + result);
+        } catch (Exception e) { // java.lang.NumberFormatException
+            System.out.println("숫자로 변환할 수 없는 문자열이 존재합니다.");
+        } finally {
+            System.out.println("소스코드를 다시 한번 확인해주세요.");
+        }
+    }
+}
