6ca9c3f99bcee91f233f0182159cad9bfb8782a2
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-04-28 14:33:04 +0900

    Commit

--- Study/src/calc/Calc.java
+++ Study/src/calc/Calc.java
@@ -3,11 +3,15 @@ package calc;
 public interface Calc {
     int ERROR = -999999999;
 
+    // 덧셈
     int add(int num1, int num2);
 
+    // 뺄셈
     int substract(int num1, int num2);
 
+    // 곱하기
     int time(int num1, int num2);
 
+    // 나누기
     int divide(int num1, int num2);
 }
