36f5d42b6206baff8048ad467a279180a3b18da7
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-05-18 14:56:55 +0900

    commit

---
+++ Study/src/gui/JFrameButton.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JFrameButton extends JFrame {
+    public JFrameButton() {
+        setTitle("JFrameButton");
+        setSize(500, 300);
+        setLocation(200, 300);
+        setLayout(new FlowLayout());
+        getContentPane().setBackground(Color.pink);
+        JButton btn1 = new JButton("11111");
+        JButton btn2 = new JButton("22222");
+        add(btn1);
+        add(btn2);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        JFrameButton f = new JFrameButton();
+    }
+}
